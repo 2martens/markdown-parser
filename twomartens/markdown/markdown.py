76d514dc47014b64b409e6f4c3f3f36d06934f7c
Jim Martens <github@2martens.de>
2017-04-15 15:58:00 +0200

    Finished main file
    Signed-off-by: Jim Martens <github@2martens.de>

--- twomartens/markdown/markdown.py
+++ twomartens/markdown/markdown.py
@@ -6,11 +6,14 @@
 
 import argparse
 
+from .parser import parse as markdown_parse
+from .renderer import render
 
 __version__ = "0.1.0"
 
 
 def main():
+    """The entry point of the application. Glues the various parts of the application together."""
     parser = argparse.ArgumentParser(description="Parses markdown and produces an HTML representation.")
     parser.add_argument("-r", "--renderer", dest="renderer", default="html", choices=["html"],
                         help="This describes the output format.")
@@ -18,3 +21,18 @@ def main():
     parser.add_argument("output", metavar="output", type=argparse.FileType('w'), help="The output file")
     
     args = parser.parse_args()
+
+    # load file content
+    markdown = args.input.read()
+
+    # parse markdown
+    structure = markdown_parse(markdown)
+
+    # render output
+    output = render(structure, args.renderer)
+
+    # write output
+    args.output.write(output)
+
+    # give feedback to console
+    print("The output file has been written.")
