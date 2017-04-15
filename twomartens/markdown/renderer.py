e1de3ca9d8237e36be6b22153da4e46216c322d8
Jim Martens <github@2martens.de>
2017-04-15 20:54:45 +0200

    Added docstrings to the functions
    Signed-off-by: Jim Martens <github@2martens.de>

--- twomartens/markdown/renderer.py
+++ twomartens/markdown/renderer.py
@@ -24,6 +24,8 @@ def render(structure: modgrammar.Grammar, output_format: str) -> str:
 def _html_render(structure: modgrammar.Grammar) -> str:
     """Renders the given structure in HTML and returns the finished output.
     
+    The first heading in the markdown text will be used for the HTML title element.
+    
     :param modgrammar.Grammar structure: the structure which is rendered
     """
     replacements = {"list": "ul", "quote": "blockquote"}
@@ -49,6 +51,19 @@ def _html_render(structure: modgrammar.Grammar) -> str:
 
 
 def _html_render_block(tag: str, elements: list, text: str = None, nesting: int = 1) -> str:
+    """Renders an HTML block element and returns the HTML output.
+    
+    Level 1 elements are indented by four spaces. Level 2 elements are indented by 8 spaces and so on.
+    Level 1 elements with children get their tags on a separate line. The tags of level 2 (or higher) elements are on
+    the same line with the rest of the content.
+    
+    Parameters
+    ----------
+    :param str tag: the HTML tag for this block element
+    :param list elements: a list of all the child elements
+    :param str text: the text of the block element between the HTML tags if available
+    :param int nesting: the level this block is on (e.g. direct child of body element is on level 1)
+    """
     content = (nesting * TAB_SEP) + "<" + tag + ">"
     if len(elements) == 0 and text is not None:
         content = content + text
@@ -70,6 +85,12 @@ def _html_render_block(tag: str, elements: list, text: str = None, nesting: int 
 
 
 def _html_render_item(tag: str, text: str, include_tags=True) -> str:
+    """Renders an HTML inline element and returns the HTML output.
+    
+    :param str tag: the HTML tag
+    :param str text: the text between the HTML tags
+    :param bool include_tags: True if the tags should be part of the output
+    """
     opening_tag = "<" + tag + ">"
     closing_tag = "</" + tag + ">"
     if include_tags:
@@ -79,6 +100,11 @@ def _html_render_item(tag: str, text: str, include_tags=True) -> str:
 
 
 def _extract_elements(structure: modgrammar.Grammar, replacements: map = None) -> list:
+    """Extracts the tag, text and children for all elements in the given structure and returns those in a list.
+    
+    :param modgrammar.Grammar structure: the structure from which the elements are extracted
+    :param map replacements: an optional map of replacements for found tags 
+    """
     elements = []
 
     for elem in structure.elements:
