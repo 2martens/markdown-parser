02eb5baa5fe80ac70c4f4707a53ce69bf88e9b38
Jim Martens <github@2martens.de>
2017-04-17 12:01:50 +0200

    Reordered package metadata and added required python version
    Signed-off-by: Jim Martens <github@2martens.de>

--- setup.py
+++ setup.py
@@ -14,31 +14,34 @@ version = re.search(
 
 with open("README.rst", "rb") as f:
     long_desc = f.read().decode()
-
 setup(
     name="twomartens.markdown",
+    description="Markdown parser that renders the markdown in HTML",
+    long_description=long_desc,
+    author="Jim Martens",
+    author_email="github@2martens.de",
+    url="https://github.com/frmwrk123/markdown-parser",
+    version=version,
     namespace_packages=["twomartens"],
     packages=["twomartens.markdown"],
     entry_points={
         "console_scripts": ['tm-parse-markdown = twomartens.markdown.markdown:main']
     },
-    version=version,
     package_data={"twomartens.markdown": ["templates/skeleton.*"]},
-    license="Apache Software License",
+    python_requires="~=3.5",
     install_requires=["modgrammar"],
+    license="Apache Software License",
     classifiers=[
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: Apache Software License",
         "Environment :: Console",
         "Topic :: Text Processing :: Markup",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
-    description="Markdown parser that renders the markdown in HTML",
-    long_description=long_desc,
-    author="Jim Martens",
-    author_email="github@2martens.de",
-    url="https://github.com/frmwrk123/markdown-parser",
+
 )
+
